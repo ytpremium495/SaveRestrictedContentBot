d2866dca7687d1a4cebafa7a919944357d3b75c8
ytpremium495 <127856047+ytpremium495@users.noreply.github.com>
2023-03-19 16:31:45 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
+API_ID = config("API_ID", "27824890"default=None, cast=int)
+API_HASH = config("API_HASH", "e9a93f2f9e421ba2e11901d189376eef"default=None)
+BOT_TOKEN = config("BOT_TOKEN", "5940975704:AAHvQnmcGRCLoqxSB8DNedWJVBwHV4jsKqA"default=None)
+SESSION = config("SESSION", "BQGokvoAtpOM8Gthq8Cs_djYMY0WM9XB0xTTO98WsO6_hNQaQOgVcUkY_i-M7gqB87ssv3xRIJ7l3gcUaTs3ML4eSeypx6eivxmHZ4uye-q3zA21A4f23MO5vUqHpA5uCLjpNwj3WpDYgSUsEO46-yCp7-h-0qRbMzKpcxECq1tEFFOaYI4Wc9RCNrfQrsTwsrHVr7JWB05QEsAWrzxb76J5ywotLUmu2cXaV_R50jhwCzSpwQxbDvJ89cogfX3kqVoS0hIObs49EkGD0TEjtFvTJBDIZihcwdb8uJhkU_oDmSYkbCadAPmGbI-u4mxl0MBs9ixfjFWDPY5B2M2ldYYlR5PizQAAAAF1HkH7AA"default=None)
+FORCESUB = config("FORCESUB", "bollywood_hollywoodmovies0"default=None)
 AUTH = config("AUTH", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
